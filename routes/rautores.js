94d669935be4e8c60154b525dd845755e48b4393
uo264197 <71664251+uo264197@users.noreply.github.com>
2021-03-26 10:32:15 +0100

    SDI–2021-309-sesión7-Ejercicio-complementario1

---
+++ routes/rautores.js
@@ -0,0 +1,38 @@
+module.exports = function(app, swig) {
+    app.get("/autores", function(req, res) {
+        let autores = [ {
+            "nombre" : "Autor1",
+            "grupo" : "Grupo1",
+            "rol" : "guitarrista"
+        }, {
+            "nombre" : "Autor2",
+            "grupo" : "Grupo2",
+            "rol" : "cantante"
+        }, {
+            "nombre" : "Autor3",
+            "grupo" : "Grupo1",
+            "rol" : "bateria"
+        } ];
+
+        let respuesta = swig.renderFile('views/bautores-lista.html', {
+            autores : autores
+        });
+
+        res.send(respuesta);
+    });
+
+    app.get('/autores/agregar', function (req, res) {
+        let respuesta = swig.renderFile('views/autores-agregar.html', { });
+        res.send(respuesta);
+    })
+
+    app.get('/autores/:param', function(req, res) {
+        res.redirect("/autores");
+    });
+
+    app.post("/autor", function(req, res) {
+        res.send("Autor agregado:"+req.body.nombre +"<br>"
+            +" Grupo :" +req.body.grupo + "<br>"
+            +" Rol: "+req.body.rol);
+    })
+}
