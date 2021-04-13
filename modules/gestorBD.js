fa8846390ca847023f86c3dfcb5c2bab8b526b5d
uo264197 <71664251+uo264197@users.noreply.github.com>
2021-04-14 01:09:34 +0200

    SDI–2021-309-sesión8-Ejercicio-complementario2

--- modules/gestorBD.js
+++ modules/gestorBD.js
@@ -123,6 +123,23 @@ module.exports = {
                 });
             }
         });
+    },
+    borrarComentario : function(criterio, funcionCallback) {
+        this.mongo.MongoClient.connect(this.app.get('db'), function(err, db) {
+            if (err) {
+                funcionCallback(null);
+            } else {
+                let collection = db.collection('comentarios');
+                collection.deleteOne(criterio, function(err, result) {
+                    if (err) {
+                        funcionCallback(null);
+                    } else {
+                        funcionCallback(result);
+                    }
+                    db.close();
+                });
+            }
+        });
     }
 
 };
--- routes/rcomentarios.js
+++ routes/rcomentarios.js
@@ -18,4 +18,19 @@ module.exports = function(app, swig, gestorBD) {
             }
         });
     })
+
+    app.post('/comentario/borrar/:id', function(req,res) {
+        if ( req.session.usuario != req.body.autor){
+            res.send("Error al borrar comentario, no es un comentario del usuario conectado"); return;
+        }
+
+        let criterio = { "_id" : gestorBD.mongo.ObjectID(req.params.id) };
+        gestorBD.borrarComentario(criterio, function(result) {
+            if (result == null) {
+                res.send("Error al borrar ");
+            } else {
+                res.send("Comentario borrado");
+            }
+        });
+    })
 }
