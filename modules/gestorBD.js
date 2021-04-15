deba4e495846b5acaa9c1f5f0f0f65352bd8383d
uo264197 <71664251+uo264197@users.noreply.github.com>
2021-04-15 13:08:13 +0200

    SDI-2021-309-9.1-Gestión de colecciones, eliminar documento.

--- modules/gestorBD.js
+++ modules/gestorBD.js
@@ -90,6 +90,22 @@ module.exports = {
             }
         });
     },
+    eliminarCancion : function(criterio, funcionCallback) {
+        this.mongo.MongoClient.connect(this.app.get('db'), function(err, db) {
+            if (err) {
+                funcionCallback(null);
+            } else {
+                let collection = db.collection('canciones');
+                collection.remove(criterio, function(err, result) {
+                    if (err) { funcionCallback(null);
+                    } else {
+                        funcionCallback(result);
+                    }
+                    db.close();
+                });
+            }
+        });
+    },
     insertarComentario : function(comentario, funcionCallback) {
         this.mongo.MongoClient.connect(this.app.get('db'), function(err, db) {
             if (err) {
@@ -135,6 +151,40 @@ module.exports = {
                         funcionCallback(null);
                     } else {
                         funcionCallback(result);
+                    }
+                    db.close();
+                });
+            }
+        });
+    },
+    insertarFavorito : function(favorita, funcionCallback) {
+        this.mongo.MongoClient.connect(this.app.get('db'), function(err, db) {
+            if (err) {
+                funcionCallback(null);
+            } else {
+                let collection = db.collection('favoritos');
+                collection.insertOne(favorita, function(err, result) {
+                    if (err) {
+                        funcionCallback(null);
+                    } else {
+                        funcionCallback(result.ops[0]._id);
+                    }
+                    db.close();
+                });
+            }
+        });
+    },
+    obtenerFavoritos : function(criterio,funcionCallback){
+        this.mongo.MongoClient.connect(this.app.get('db'), function(err, db) {
+            if (err) {
+                funcionCallback(null);
+            } else {
+                let collection = db.collection('favoritos');
+                collection.find(criterio).toArray(function(err, favoritos) {
+                    if (err) {
+                        funcionCallback(null);
+                    } else {
+                        funcionCallback(favoritos);
                     }
                     db.close();
                 });
---
+++ routes/rfavoritos.js
@@ -0,0 +1,41 @@
+module.exports = function(app, swig, gestorBD) {
+
+    app.get('/favoritos', function(req,res) {
+        let criterio = { autor : req.session.usuario };
+        gestorBD.obtenerFavoritos(criterio, function(canciones) {
+            if (canciones == null) {
+                res.send("Error al mostrar favoritos");
+            } else {
+                let respuesta = swig.renderFile('views/bfavoritos.html',
+                    {
+                        canciones : canciones
+                    });
+                res.send(respuesta);
+            }
+        });
+    })
+
+    app.get('/favoritos/add/:id', function(req,res) {
+        if ( req.session.usuario == null){
+            res.redirect("/tienda"); return;
+        }
+        let favorito = {
+            id : req.body.id,
+            nombre : req.body.nombre,
+            precio : req.body.precio,
+            autor : req.session.usuario
+        }
+
+        // Conectarse
+        gestorBD.insertarFavorito(favorito, function(id){
+            if (id == null) {
+                res.send("Error al insertar canción");
+            } else {
+                res.send("Agregada id: "+ id);
+            }
+        });
+    })
+
+
+
+}
