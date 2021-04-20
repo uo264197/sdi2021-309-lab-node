070f7ac19d6f2ce1585350edc7d5f6df77f0758c
uo264197 <71664251+uo264197@users.noreply.github.com>
2021-04-20 17:07:47 +0200

    SDI–2021-309-10.6-SW Servicio Memoria.

--- hello-world-server.js
+++ hello-world-server.js
@@ -1,6 +1,8 @@
 let http = require('http');
 http.createServer(function handler(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World\n');
+    setTimeout(function() { // Espera de 10 segundos
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end('Hello World\n');
+    }, 10000);
 }).listen(1337, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:1337/');
